a23da93ad4fe957ea7f8cd48dbb6a7115ec48661
Vitali Minor <gremi78-coursera@yahoo.de>
2013-07-09 22:17:37 +0000

    Changed web.js to read from index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buffer = new Buffer(fs.readFileSync("index.html"));
+    var buffer = new Buffer(fs.readFileSync('./index.html', 'utf-8'));
     response.send(buffer.write());
 });
 
